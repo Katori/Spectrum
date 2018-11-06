339ffded01e05f1ac65b536f98a0793fb2b4858d
Zac North <znorth@gmail.com>
2018-11-06 03:07:41 -0500

    Continued development of NetMan2 architecture

--- Assets/Spectrum/Scripts/NetMan2/GameServerNetMan2.cs
+++ Assets/Spectrum/Scripts/NetMan2/GameServerNetMan2.cs
@@ -2,6 +2,15 @@
 {
 	public class GameServerNetMan2 : NetMan2
 	{
-		
+		public GameNetworkManager GameNetMan;
+
+		private void Start()
+		{
+			GameNetMan.networkAddress = "10.1.10.65";
+			GameNetMan.serverBindAddress = "10.1.10.65";
+			GameNetMan.serverBindToIP = true;
+			GameNetMan.networkPort = 7790;
+			GameNetMan.StartServer();
+		}
 	}
 }
